b5750d9d0f21ac18a2574c8d83cbbbe4252345d5
indrabushan <72606082+indrabushan@users.noreply.github.com>
2020-10-09 16:40:45 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,22 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram File convert Bot!
-You can convert any Telegram Document, using this bot! With custom thumbnail
-     
-    """
+Buddy I am a Telegram File convert Bot maintained by my master @SI_NN_ER_LS.
+
+By using me your can add coustom thumbnail to your video.
+
+To find out more about how to use me click➡️ /help
+ """
     ABS_TEXT = " Please don't be selfish."
     BANNED_USER_TEXT = "you are banned"
     UPGRADE_TEXT = "No upgrade plan 🥰"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "trying to download📥"
+    UPLOAD_START = "trying to upload📤"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully🖼️."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = ""
@@ -24,18 +26,17 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
 Telegram ID: <code>{}</code>
 Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
 Expires on: {}"""
-    HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
---------
+    HELP_USER = """Here we go it's simple to use me
+👉 FOLLOW THE STEPS
 
-🔥 How to deploy : <a href='https://youtu.be/zQamSjXBpJU'>Click here</a>
+1)Send me a coustom thumbnail
+2)Send the file that you need to convert as video and reply to it with /converttovideo.
+3)use /deletethumbnail to remove saved thumbnail
 
-    © Group   : @InfotelGroup
-    © Channel : @DX_BotZ 
-    © credits : @Spechide
 
-Send /me to know current plan details
+If you like be make Shure to subscribe our channel👉 @BlueSkyMovie.
 
+Enjoy🐙🐍
 """
 
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
